5b447d45fc9581f61308848b5f1b20ac529234d7
Shaoting <shaotingf@uchicago.edu>
2024-10-17 03:35:33 -0500

    Change port

--- tests/tests.py
+++ tests/tests.py
@@ -69,7 +69,7 @@ def ModelConfig(model: str, BootstrapConfig) -> None:
 ##### Test cases #####
 def test_multi_turn(model = "mistralai/Mistral-7B-Instruct-v0.2") -> pd.DataFrame:
     # Start one server: with lmcache; for contrast (not saving decode KV Cache), change save_decode_cache to false
-    config = CreateSingleLocalBootstrapConfig(8002, 0, model, "configs/lmcache_local_cpu_multi.yaml")
+    config = CreateSingleLocalBootstrapConfig(8000, 0, model, "configs/lmcache_local_cpu_multi.yaml")
 
     # Set vllm configuration for different models
     ModelConfig(model, config)
