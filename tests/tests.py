785d1ab47dd2ab1fa95367222cb456825c7d352c
Shaoting Feng <shaotingf@nature.cs.uchicago.edu>
2024-10-17 03:28:23 -0500

    [Add] add tests for multi turn conversation

--- tests/tests.py
+++ tests/tests.py
@@ -38,6 +38,16 @@ def CreateDummyExperiment(num_requests, context_length, gap_between_requests = 8
     cfg = WorkloadConfig(qps, duration, context_length, 16, offset = 0)
     return (cfg, Usecase.DUMMY)
 
+def CreateMultiTurnExperiment(num_requests, context_length, gap_between_requests = 8):
+    """
+    Create some requests for multi-turn conversation
+    The query length will be 16
+    """
+    qps = 1 / gap_between_requests
+    duration = num_requests * gap_between_requests
+    cfg = WorkloadConfig(qps, duration, context_length, 16, offset = 0)
+    return (cfg, Usecase.MULTI)
+
 def ModelConfig(model: str, BootstrapConfig) -> None:
     """
     Set configuration for bootstrap for different models
@@ -48,7 +58,6 @@ def ModelConfig(model: str, BootstrapConfig) -> None:
         case "THUDM/glm-4-9b-chat":
             BootstrapConfig.vllm_config.tensor_parallel_size = 2
             BootstrapConfig.vllm_config.gpu_memory_utilization = 0.8
-            BootstrapConfig.envs = {}
             BootstrapConfig.vllm_optional_config["trust_remote_code"] = ""
         case "meta-llama/Llama-3.1-8B-Instruct":
             BootstrapConfig.vllm_optional_config["enable_chunked_prefill"] = False
@@ -58,6 +67,25 @@ def ModelConfig(model: str, BootstrapConfig) -> None:
 
 
 ##### Test cases #####
+def test_multi_turn(model = "mistralai/Mistral-7B-Instruct-v0.2") -> pd.DataFrame:
+    # Start one server: with lmcache; for contrast (not saving decode KV Cache), change save_decode_cache to false
+    config = CreateSingleLocalBootstrapConfig(8002, 0, model, "configs/lmcache_local_cpu_multi.yaml")
+
+    # Set vllm configuration for different models
+    ModelConfig(model, config)
+
+    # Experiment: ONE query that contains 5 rounds 
+    lengths = [16]
+    experiments = [CreateMultiTurnExperiment(1, length ) for length in lengths]
+
+    test_case = TestCase(
+            experiments = experiments,
+            engines = [config])
+    
+    # Run test case
+    final_result = run_test_case(test_case)
+    return final_result
+
 def test_lmcache_local_gpu(model = "mistralai/Mistral-7B-Instruct-v0.2") -> pd.DataFrame:
     # Start two servers: with lmcache and without lmcache
     config1 = CreateSingleLocalBootstrapConfig(8000, 0, model, "configs/lmcache_local_gpu.yaml")
@@ -259,36 +287,6 @@ def test_lmcache_remote_disk(model = "mistralai/Mistral-7B-Instruct-v0.2") -> pd
     # Run test case
     final_result = run_test_case(test_case)
     return final_result
-
-# def test_lmcache_chatglm() -> pd.DataFrame:
-#     # Start two servers: with lmcache and without lmcache
-#     config1 = CreateSingleLocalBootstrapConfig(8000, 0, "THUDM/glm-4-9b-chat", "configs/lmcache_remote_cachegen.yaml")
-#     config2 = CreateSingleLocalBootstrapConfig(8001, 1, "THUDM/glm-4-9b-chat", None)
-
-#     config1.vllm_config.tensor_parallel_size = 2
-#     config1.vllm_config.gpu_memory_utilization = 0.8
-#     config1.envs = {}
-#     config1.vllm_optional_config["trust_remote_code"] = ""
-
-#     config2.vllm_config.tensor_parallel_size = 2
-#     config2.vllm_config.gpu_memory_utilization = 0.8
-#     config2.envs = {}
-#     config2.vllm_optional_config["trust_remote_code"] = ""
-
-#     # Experiments: 8K, 16K, 24K shared context, each experiments has 5 queries
-#     lengths = [8192, 16384, 24576]
-#     experiments = [CreateDummyExperiment(10, length) for length in lengths]
-
-#     test_case1 = TestCase(
-#             experiments = experiments,
-#             engines = [config1])
-
-#     test_case2 = TestCase(
-#             experiments = experiments,
-#             engines = [config2])
-
-#     # Run test case
-#     return run_test_cases([test_case1, test_case2])
 
 def test_lmcache_redis_sentinel(model = "mistralai/Mistral-7B-Instruct-v0.2") -> pd.DataFrame:
     config1 = CreateSingleLocalBootstrapConfig(8000, 1, model, "configs/lmcache_redis_sentinel_cachegen.yaml")
