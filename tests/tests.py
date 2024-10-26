36b24776ce1895041f711729d71e1a93cb930798
Shaoting Feng <shaotingf@nature.cs.uchicago.edu>
2024-10-25 21:12:59 -0500

    bug fix simple_test

--- tests/tests.py
+++ tests/tests.py
@@ -81,10 +81,8 @@ def ModelConfig(model: str, BootstrapConfig) -> None:
 def simple_test(model = "mistralai/Mistral-7B-Instruct-v0.2") -> pd.DataFrame:
     config = CreateSingleLocalBootstrapConfig(8002, 0, model, "configs/lmcache_local_cpu.yaml")
     ModelConfig(model, config)
-    # lengths = [8192]
-    # experiments = [CreateDummyExperiment(3, length ) for length in lengths]
-    lengths = [12288, 24576, 24576]
-    experiments = [CreateVaryLengthExperiment(len(lengths), lengths )]
+    lengths = [8192]
+    experiments = [CreateDummyExperiment(3, length ) for length in lengths]
     test_case = TestCase(
             experiments = experiments,
             engines = [config])
